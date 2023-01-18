99a31eb6aff97eeacb979738c8dcee493e26f7c3
Ciara Fennessy <ciara.fennessy@weareams.com>
2023-01-18 11:22:15 +0000

    created backend repo

---
+++ target/generated-sources/openapi/src/main/java/org/springframework/samples/petclinic/rest/dto/VetDto.java
@@ -0,0 +1,173 @@
+package org.springframework.samples.petclinic.rest.dto;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.samples.petclinic.rest.dto.SpecialtyDto;
+import org.springframework.samples.petclinic.rest.dto.VetAllOfDto;
+import org.springframework.samples.petclinic.rest.dto.VetFieldsDto;
+import java.time.OffsetDateTime;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import org.hibernate.validator.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import javax.annotation.Generated;
+
+/**
+ * A veterinarian.
+ */
+
+@Schema(name = "Vet", description = "A veterinarian.")
+@JsonTypeName("Vet")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2023-01-16T17:38:53.901290Z[Europe/Dublin]")
+public class VetDto {
+
+  @JsonProperty("firstName")
+  private String firstName;
+
+  @JsonProperty("lastName")
+  private String lastName;
+
+  @JsonProperty("specialties")
+  @Valid
+  private List<SpecialtyDto> specialties = new ArrayList<>();
+
+  @JsonProperty("id")
+  private Integer id;
+
+  public VetDto firstName(String firstName) {
+    this.firstName = firstName;
+    return this;
+  }
+
+  /**
+   * The first name of the vet.
+   * @return firstName
+  */
+  @NotNull @Pattern(regexp = "^[a-zA-Z]*$") @Size(min = 1, max = 30) 
+  @Schema(name = "firstName", example = "James", description = "The first name of the vet.", required = true)
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public VetDto lastName(String lastName) {
+    this.lastName = lastName;
+    return this;
+  }
+
+  /**
+   * The last name of the vet.
+   * @return lastName
+  */
+  @NotNull @Pattern(regexp = "^[a-zA-Z]*$") @Size(min = 1, max = 30) 
+  @Schema(name = "lastName", example = "Carter", description = "The last name of the vet.", required = true)
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public VetDto specialties(List<SpecialtyDto> specialties) {
+    this.specialties = specialties;
+    return this;
+  }
+
+  public VetDto addSpecialtiesItem(SpecialtyDto specialtiesItem) {
+    if (this.specialties == null) {
+      this.specialties = new ArrayList<>();
+    }
+    this.specialties.add(specialtiesItem);
+    return this;
+  }
+
+  /**
+   * The specialties of the vet.
+   * @return specialties
+  */
+  @NotNull @Valid 
+  @Schema(name = "specialties", description = "The specialties of the vet.", required = true)
+  public List<SpecialtyDto> getSpecialties() {
+    return specialties;
+  }
+
+  public void setSpecialties(List<SpecialtyDto> specialties) {
+    this.specialties = specialties;
+  }
+
+  public VetDto id(Integer id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * The ID of the vet.
+   * minimum: 0
+   * @return id
+  */
+  @Min(0) 
+  @Schema(name = "id", accessMode = Schema.AccessMode.READ_ONLY, example = "1", description = "The ID of the vet.", required = true)
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    VetDto vet = (VetDto) o;
+    return Objects.equals(this.firstName, vet.firstName) &&
+        Objects.equals(this.lastName, vet.lastName) &&
+        Objects.equals(this.specialties, vet.specialties) &&
+        Objects.equals(this.id, vet.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(firstName, lastName, specialties, id);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class VetDto {\n");
+    sb.append("    firstName: ").append(toIndentedString(firstName)).append("\n");
+    sb.append("    lastName: ").append(toIndentedString(lastName)).append("\n");
+    sb.append("    specialties: ").append(toIndentedString(specialties)).append("\n");
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
